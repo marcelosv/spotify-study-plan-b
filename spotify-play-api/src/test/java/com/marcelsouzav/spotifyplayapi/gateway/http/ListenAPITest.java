346782dc756604dc45562d04aca3fbf9ceba5d75
Marcelo <marcelo@Marcelos-MacBook-Air.local>
2018-11-12 09:13:20 -0200

    i created first service for listener music

---
+++ spotify-play-api/src/test/java/com/marcelsouzav/spotifyplayapi/gateway/http/ListenAPITest.java
@@ -0,0 +1,30 @@
+package com.marcelsouzav.spotifyplayapi.gateway.http;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ListenAPI.class)
+public class ListenAPITest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void
+    test_get_music_with_return_ok() throws Exception {
+        this.mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/v1/music/")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+}
